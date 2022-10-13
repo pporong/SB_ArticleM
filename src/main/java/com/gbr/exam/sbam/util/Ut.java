178def1a306e12f29f4d16e08dcabd54eac228f4
pporong <inane09@gmail.com>
2022-10-13 17:50:22 +0900

    Ut.empty 함수 도입

---
+++ src/main/java/com/gbr/exam/sbam/util/Ut.java
@@ -0,0 +1,18 @@
+package com.gbr.exam.sbam.util;
+
+public class Ut {
+	public static boolean empty(Object obj) {
+
+		if (obj == null) {
+			return true;
+		}
+
+		if (obj instanceof String == false) {
+			return true;
+		}
+
+		String str = (String) obj;
+
+		return str.trim().length() == 0;
+	}
+}
