d61d0388bbd2c45337a87939f680f4d88c613004
pporong <inane09@gmail.com>
2022-10-13 17:36:39 +0900

    MemberRepository 추가, Insert 되도록 처리

---
+++ src/main/java/com/gbr/exam/sbam/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package com.gbr.exam.sbam.repository;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberRepository {
+	@Insert("""
+				INSERT INTO `member`
+				SET regDate = NOW(),
+				updateDate = NOW(),
+				loginId = #{loginId},
+				loginPw = #{loginPw},
+				`name` = #{name},
+				nickname = #{nickname},
+				cellphoneNum = #{cellphoneNum},
+				email = #{email}
+			""")
+	void join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email);
+
+}
--- src/main/java/com/gbr/exam/sbam/service/UserMemberService.java
+++ src/main/java/com/gbr/exam/sbam/service/UserMemberService.java
@@ -2,14 +2,20 @@ package com.gbr.exam.sbam.service;
 
 import org.springframework.stereotype.Service;
 
+import com.gbr.exam.sbam.repository.MemberRepository;
+
 @Service
 public class UserMemberService {
 
-//	private MemberRepository memberRepository;
+	private MemberRepository memberRepository;
+
+	public UserMemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
 		
-//	memberRepository.join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email);
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNum, email);
 
 	}
 }
