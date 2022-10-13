1d3154985dfa774c12ed4ff1abaf021c31394a95
pporong <inane09@gmail.com>
2022-10-13 16:13:10 +0900

    writeArticle 메서드 적용, 게시물 작성로직 중복제거

--- src/main/java/com/gbr/exam/sbam/controller/UserArticleController.java
+++ src/main/java/com/gbr/exam/sbam/controller/UserArticleController.java
@@ -23,17 +23,30 @@ public class UserArticleController {
 
 	private void makeTestData() {
 		for (int i = 1; i <= 10; i++) {
-			int id = lastArticleId + 1;
 			String title = "제목 " + i;
 			String body = "내용 " + i;
 
-			Article article = new Article(id, title, body);
-
-			articles.add(article);
-			lastArticleId = id;
+			writeArticle(title, body);
 		}
 	}
 	
+	private Article writeArticle(String title, String body) {
+		int id = lastArticleId + 1;
+		Article article = new Article(id, title, body);
+
+		articles.add(article);
+		lastArticleId = id;
+		return article;
+	}
+
+	@RequestMapping("/user/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
+
+		return article;
+	}
+
 	@RequestMapping("/user/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
