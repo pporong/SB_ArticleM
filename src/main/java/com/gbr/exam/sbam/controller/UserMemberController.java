b5499d7f3f419c1dd32c99bcaf5211bfee109ea2
pporong <inane09@gmail.com>
2022-10-13 17:47:38 +0900

    회원가입시 입력된 정보의 유효성 체크

--- src/main/java/com/gbr/exam/sbam/controller/UserMemberController.java
+++ src/main/java/com/gbr/exam/sbam/controller/UserMemberController.java
@@ -17,6 +17,26 @@ public class UserMemberController {
 	@RequestMapping("user/member/doJoin")
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+
+		if (loginId == null || loginId.trim().length() == 0) {
+			return "!! 아이디를 입력 해 주세요 !!";
+		}
+		if (loginPw == null || loginPw.trim().length() == 0) {
+			return "!! 비밀번호를 입력 해 주세요 !!";
+		}
+		if (name == null || name.trim().length() == 0) {
+			return "!! 이름을 입력 해 주세요 !!";
+		}
+		if (nickname == null || nickname.trim().length() == 0) {
+			return "!! 닉네임을 입력 해 주세요 !!";
+		}
+		if (cellphoneNum == null || cellphoneNum.trim().length() == 0) {
+			return "!! 전화번호를 입력 해 주세요 !!";
+		}
+		if (email == null || email.trim().length() == 0) {
+			return "!! 이메일을 입력 해 주세요 !!";
+		}
+		
 		int id = userMemberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
 
 		if (id == -1) {
