f2e64203453ce2174156fa9f692e89858fc88e4b
pporong <inane09@gmail.com>
2022-10-13 18:14:48 +0900

    각 메서드에 ResultData 적용

--- src/main/java/com/gbr/exam/sbam/controller/UserArticleController.java
+++ src/main/java/com/gbr/exam/sbam/controller/UserArticleController.java
@@ -22,18 +22,29 @@ public class UserArticleController {
 	// 액션메서드 
 	@RequestMapping("/user/article/doAdd")
 	@ResponseBody
-	public Article doAdd(String title, String body) {
-		int id = userArticleService.writeArticle(title, body);
+	public ResultData doAdd(String title, String body) {
+		if (Ut.empty(title)) {
+			return ResultData.from("F-1", "제목을 입력해주세요");
+		}
+		if (Ut.empty(body)) {
+			return ResultData.from("F-2", "내용을 입력해주세요");
+		}
 
+		ResultData writeArticleRd = userArticleService.writeArticle(title, body);
+
+		int id = (int) writeArticleRd.getData1();
+		
 		Article article = userArticleService.getArticle(id);
 
-		return article;
+		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
 	}
 
 	@RequestMapping("/user/article/getArticles")
 	@ResponseBody
-	public List<Article> getArticles() {
-		return userArticleService.getArticles();
+	public ResultData getArticles() {
+		List<Article> articles = userArticleService.getArticles();
+
+		return ResultData.from("S-1", "Article List", articles);
 	}
 	
 
--- src/main/java/com/gbr/exam/sbam/service/UserMemberService.java
+++ src/main/java/com/gbr/exam/sbam/service/UserMemberService.java
@@ -3,7 +3,9 @@ package com.gbr.exam.sbam.service;
 import org.springframework.stereotype.Service;
 
 import com.gbr.exam.sbam.repository.MemberRepository;
+import com.gbr.exam.sbam.util.Ut;
 import com.gbr.exam.sbam.vo.Member;
+import com.gbr.exam.sbam.vo.ResultData;
 
 @Service
 public class UserMemberService {
@@ -14,24 +16,27 @@ public class UserMemberService {
 		this.memberRepository = memberRepository;
 	}
 
-	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+	public ResultData join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
 		
 		Member existsMember = getMemberByLoginId(loginId);
 
 		// id 중복체크
 		if (existsMember != null) {
-			return -1;
+			return ResultData.from("F-7", Ut.f("이미 사용중인 아이디(%s)입니다", loginId));
 		}
 		
 		// name + email 중복체크
 		existsMember = getMemberByNameAndEmail(name, email);
 
 		if (existsMember != null) {
-			return -2;
+			return ResultData.from("F-8", Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다", name, email));
 		}
 		
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNum, email);
-		return memberRepository.getLastInsertId();
+		
+		int id = memberRepository.getLastInsertId();
+
+		return ResultData.from("S-1", "회원가입이 완료되었습니다", id);
 	}
 	
 	private Member getMemberByNameAndEmail(String name, String email) {
