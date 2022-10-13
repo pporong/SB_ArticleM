f0261869025468e7095b86c6288de841a9bcb09c
pporong <inane09@gmail.com>
2022-10-13 16:20:07 +0900

    게시물 수정기능 구현

--- src/main/java/com/gbr/exam/sbam/controller/UserArticleController.java
+++ src/main/java/com/gbr/exam/sbam/controller/UserArticleController.java
@@ -42,12 +42,6 @@ public class UserArticleController {
 
 		return null;
 	}
-
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-
-		articles.remove(article);
-	}
 	
 	private Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
@@ -56,6 +50,19 @@ public class UserArticleController {
 		articles.add(article);
 		lastArticleId = id;
 		return article;
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+
+		articles.remove(article);
+	}
+
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		article.setTitle(title);
+		article.setBody(body);
 	}
 
 	// 액션메서드 
@@ -80,11 +87,26 @@ public class UserArticleController {
 		Article article = getArticle(id);
 
 		if (article == null) {
-			return id + " 번 게시물은 존재하지 않습니다. :( )";
+			return id + " 번 게시물은 존재하지 않습니다. :(";
 		}
 
 		deleteArticle(id);
 
 		return id + " 번 게시물이 삭제되었습니다. :)";
 	}
+	
+	@RequestMapping("/user/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + " 번 게시물은 존재하지 않습니다. :(";
+		}
+
+		modifyArticle(id, title, body);
+
+		return id + " 번 게시물이 수정되었습니다. :)";
+	}
+
 }
