ee62fff891df9333add6e54c8ce59449c2795691
pporong <inane09@gmail.com>
2022-10-13 18:00:19 +0900

    getArticle에 ResultData적용

--- src/main/java/com/gbr/exam/sbam/controller/UserArticleController.java
+++ src/main/java/com/gbr/exam/sbam/controller/UserArticleController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gbr.exam.sbam.service.UserArticleService;
+import com.gbr.exam.sbam.util.Ut;
 import com.gbr.exam.sbam.vo.Article;
+import com.gbr.exam.sbam.vo.ResultData;
 
 @Controller
 public class UserArticleController {
@@ -65,14 +67,14 @@ public class UserArticleController {
 
 	@RequestMapping("/user/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article = userArticleService.getArticle(id);
 
 		if (article == null) {
-			return id + " 번 게시물은 존재하지 않습니다. :(";
+			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다. :(", id));
 		}
 
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
 	}
 
 }
