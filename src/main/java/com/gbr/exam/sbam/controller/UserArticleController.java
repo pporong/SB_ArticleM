b61a2893dd0029c5cd199175a52d3e309fdc19ad
pporong <inane09@gmail.com>
2022-10-14 15:19:15 +0900

    ResultData 에 data1Name 추가

--- src/main/java/com/gbr/exam/sbam/controller/UserArticleController.java
+++ src/main/java/com/gbr/exam/sbam/controller/UserArticleController.java
@@ -50,7 +50,7 @@ public class UserArticleController {
 		
 		Article article = userArticleService.getArticle(id);
 
-		return ResultData.newData(writeArticleRd, article);
+		return ResultData.newData(writeArticleRd, "article", article);
 	}
 
 	@RequestMapping("/user/article/getArticles")
@@ -58,7 +58,7 @@ public class UserArticleController {
 	public ResultData<List<Article>> getArticles() {
 		List<Article> articles = userArticleService.getArticles();
 
-		return ResultData.from("S-1", "Article List", articles);
+		return ResultData.from("S-1", "Article List", "articles", articles);
 	}
 	
 
@@ -81,7 +81,7 @@ public class UserArticleController {
 		Article article = userArticleService.getArticle(id);
 
 		if (article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다 :(", id), id);
+			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다 :(", id), "id", id);
 		} 
 		// 삭제 권한 체크
 		if (article.getMemberId() != loginedMemberId) {
@@ -90,7 +90,7 @@ public class UserArticleController {
 
 		userArticleService.deleteArticle(id);
 
-		return ResultData.from("S-1", Ut.f("%d번 게시물이 삭제되었습니다. :)", id), id);
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 삭제되었습니다. :)", id), "id", id);
 	}
 	
 	@RequestMapping("/user/article/doModify")
@@ -112,7 +112,7 @@ public class UserArticleController {
 		Article article = userArticleService.getArticle(id);
 
 		if (article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다 :(", id), id);
+			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다 :(", id), "id", id);
 		}
 
 		ResultData actorCanModifyRd = userArticleService.actorCanModify(loginedMemberId, article);
@@ -134,7 +134,7 @@ public class UserArticleController {
 			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다. :(", id));
 		}
 
-		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), "article", article);
 	}
 
 }
--- src/main/java/com/gbr/exam/sbam/controller/UserMemberController.java
+++ src/main/java/com/gbr/exam/sbam/controller/UserMemberController.java
@@ -49,7 +49,7 @@ public class UserMemberController {
 	
 		Member member = userMemberService.getMemberById(joinRd.getData1());
 
-		return ResultData.newData(joinRd, member);
+		return ResultData.newData(joinRd, "member", member);
 	}
 	
 	@RequestMapping("user/member/doLogin")
